09f1aa7e7271003137dcd4161591824efa63a101
Ghevi <ghevi96@gmail.com>
2020-08-09 22:38:08 +0200

    Finished project

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,12 +1,49 @@
 import { Modal } from './UI/Modal';
+import { Map } from './UI/Map';
+import { getCoordsFromAddress, getAddressFromCoords } from './Utility/Location';
 
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector('form');
     const locateUserBtn = document.getElementById('locate-btn');
+    this.shareBtn = document.getElementById('share-btn');
 
-    locateUserBtn.addEventListener('click', this.onLocateUser);
-    addressForm.addEventListener('submit', this.onFindAddress);
+    locateUserBtn.addEventListener('click', this.onLocateUser.bind(this));
+    this.shareBtn.addEventListener('click', this.onSharePlace);
+    addressForm.addEventListener('submit', this.onFindAddress.bind(this));
+  }
+
+  onSharePlace() {
+    const sharedLinkInputEl = document.getElementById('share-link');
+
+    if (!navigator.clipboard) {
+      sharedLinkInputEl.select();
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(sharedLinkInputEl.value)
+      .then(() => {
+        alert('Copied into cliploard.');
+      })
+      .catch((error) => {
+        console.log(error);
+        sharedLinkInputEl.select();
+      });
+  }
+
+  selectPlace(coordinates, address) {
+    if (this.map) {
+      this.map.render(coordinates);
+    } else {
+      this.map = new Map(coordinates);
+    }
+
+    this.shareBtn.disabled = false;
+    const sharedLinkInputEl = document.getElementById('share-link');
+    sharedLinkInputEl.value = `${location.origin}/my-place?address=${encodeURI(
+      address
+    )}&lat=${coordinates.lat}&lng=${coordinates.lng}`;
   }
 
   onLocateUser() {
@@ -17,17 +54,22 @@ class PlaceFinder {
       return;
     }
 
-    const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
+    const modal = new Modal(
+      'loading-modal-content',
+      'Loading location - please wait!'
+    );
     modal.show();
 
     navigator.geolocation.getCurrentPosition(
-      (successResult) => {
-        modal.hide();
+      async (successResult) => {
         const coordinates = {
           lat: successResult.coords.latitude,
           lng: successResult.coords.longitude,
         };
-        console.log(coordinates);
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        // console.log(coordinates);
+        this.selectPlace(coordinates, address);
       },
       (error) => {
         modal.hide();
@@ -38,7 +80,30 @@ class PlaceFinder {
     );
   }
 
-  onFindAddress() {}
+  async onFindAddress(event) {
+    event.preventDefault();
+    const address = event.target.querySelector('input').value;
+    if (!address || address.trim().length === 0) {
+      alert('Invalid address - please enter a valid one.');
+      return;
+    }
+
+    const modal = new Modal(
+      'loading-modal-content',
+      'Loading location - please wait!'
+    );
+    modal.show();
+
+    // Without async await: getCoordsFromAddress(address).then(...).catch(...);
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates, address);
+    } catch (error) {
+      alert(error.message);
+    }
+
+    modal.hide();
+  }
 }
 
 const placeFinder = new PlaceFinder();
---
+++ src/Utility/Location.js
@@ -0,0 +1,38 @@
+const GOOGLE_APY_KEY = '';
+
+export async function getAddressFromCoords(coords) {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${GOOGLE_APY_KEY}`
+  );
+
+  if (!response) {
+    throw new Error('Failed to fetch address - please try again.');
+  }
+
+  const data = await response.json();
+  if (data.error_message) {
+    throw new Error(data.error_message);
+  }
+
+  const address = data.results[0].formatted_address;
+  return address;
+}
+
+export async function getCoordsFromAddress(address) {
+  const urlAddress = encodeURI(address); // Get converted to url friendly encoding
+
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${urlAddress}&key=${GOOGLE_APY_KEY}`
+  );
+  if (!response) {
+    throw new Error('Failed to fetch coordinates - please try again.');
+  }
+
+  const data = await response.json();
+  if (data.error_message) {
+    throw new Error(data.error_message);
+  }
+
+  const coordinates = data.results[0].geometry.location;
+  return coordinates;
+}
