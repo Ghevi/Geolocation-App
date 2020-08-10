c2080afec50c49275562f501ff4d7bd676d9a9e1
Ghevi <ghevi96@gmail.com>
2020-08-10 22:43:44 +0200

    Added key

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -2,8 +2,12 @@ import { Modal } from './UI/Modal';
 import { Map } from './UI/Map';
 import { getCoordsFromAddress, getAddressFromCoords } from './Utility/Location';
 
+// import { GOOGLE_APY_KEY } from './api-key';
+
 class PlaceFinder {
   constructor() {
+    // document.querySelector('script').src += GOOGLE_APY_KEY;
+
     const addressForm = document.querySelector('form');
     const locateUserBtn = document.getElementById('locate-btn');
     this.shareBtn = document.getElementById('share-btn');
--- src/Utility/Location.js
+++ src/Utility/Location.js
@@ -1,4 +1,4 @@
-const GOOGLE_APY_KEY = '';
+const GOOGLE_APY_KEY = 'AIzaSyAyTQpOpygH6Qg8QMJQSvcjrhes5fOydmo'
 
 export async function getAddressFromCoords(coords) {
   const response = await fetch(
